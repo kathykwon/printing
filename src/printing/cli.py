e060ac8cbd673c73aa7968a8ae57e3fef81c8529
kathykwon <kwonkatherinee@gmail.com>
2024-07-13 09:43:19 -0400

    Refactor and use use spark dfs

---
+++ src/printing/cli.py
@@ -0,0 +1,182 @@
+import argparse
+import logging
+from pprint import pprint
+
+from printing.printing import Printing
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+
+DEFAULT = ["MSFT", "AAPL", "TSLA", "NVDA", "AMD"]
+
+
+class PrintingCli:
+    def get_arg_parser(self) -> argparse.ArgumentParser:
+        parser = argparse.ArgumentParser()
+        subparsers = parser.add_subparsers(
+            metavar="cmd", help="The endpoint to hit.", required=True
+        )
+        self._add_get_price(subparsers)
+        self._add_get_news(subparsers)
+        self._add_get_insider_info(subparsers)
+        self._add_get_financials(subparsers)
+        self._add_get_recs(subparsers)
+
+        return parser
+
+    #
+    # price
+    # Get stock price information.
+    #
+
+    def _add_get_price(
+        self, subparsers: "argparse._SubParsersAction[argparse.ArgumentParser]"
+    ) -> None:
+        subparser = subparsers.add_parser("price", help="Gets the current stock price.")
+        subparser.add_argument(
+            "--symbol",
+            required=False,
+            help=f"The ticker symbol to look at. Looks at {DEFAULT} if not set.",
+        )
+        subparser.set_defaults(func=self._do_get_price)
+
+    def _do_get_price(self, args: argparse.Namespace) -> None:
+        if args.symbol is None:
+            symbols = DEFAULT
+        else:
+            symbols = args.symbol.split(",")
+
+        for symbol in symbols:
+            stock = Printing(symbol)
+            pprint(stock.price)
+
+    #
+    # news
+    # Latest news on the stock.
+    #
+
+    def _add_get_news(
+        self, subparsers: "argparse._SubParsersAction[argparse.ArgumentParser]"
+    ) -> None:
+        subparser = subparsers.add_parser("news", help="Gets latest news.")
+        subparser.add_argument(
+            "--symbol",
+            required=False,
+            help=f"The ticker symbol to look at. Looks at {DEFAULT} if not set.",
+        )
+        subparser.set_defaults(func=self._do_get_news)
+
+    def _do_get_news(self, args: argparse.Namespace) -> None:
+        if args.symbol is None:
+            symbols = DEFAULT
+        else:
+            symbols = args.symbol.split(",")
+
+        for symbol in symbols:
+            stock = Printing(symbol)
+            stock.news
+
+    #
+    # insider
+    # Latest insider information on the stock.
+    #
+
+    def _add_get_insider_info(
+        self, subparsers: "argparse._SubParsersAction[argparse.ArgumentParser]"
+    ) -> None:
+        subparser = subparsers.add_parser("insider", help="Gets insider information.")
+        subparser.add_argument(
+            "--symbol",
+            required=False,
+            help=f"The ticker symbol to look at. Looks at {DEFAULT} if not set.",
+        )
+        subparser.add_argument(
+            "--type",
+            required=False,
+            help=f"'transacton' for insider transactions.",
+        )
+        subparser.add_argument(
+            "--count",
+            type=int,
+            required=False,
+            help=f"Max row count to display.",
+        )
+        subparser.set_defaults(func=self._do_get_insider_info)
+
+    def _do_get_insider_info(self, args: argparse.Namespace) -> None:
+        if args.symbol is None:
+            symbols = DEFAULT
+        else:
+            symbols = args.symbol.split(",")
+
+        for symbol in symbols:
+            stock = Printing(symbol)
+            stock.get_insider_info(args.type, args.count)
+
+    #
+    # financials
+    # Stock recommendations.
+    #
+
+    def _add_get_financials(
+        self, subparsers: "argparse._SubParsersAction[argparse.ArgumentParser]"
+    ) -> None:
+        subparser = subparsers.add_parser("financials", help="Gets financial information.")
+        subparser.add_argument(
+            "--symbol",
+            required=False,
+            help=f"The ticker symbol to look at. Looks at {DEFAULT} if not set.",
+        )
+        subparser.add_argument(
+            "--quarterly",
+            action="store_true",
+            help=f"Get the quarterly report if set to True.",
+        )
+        subparser.set_defaults(func=self._do_get_financials)
+
+    def _do_get_financials(self, args: argparse.Namespace) -> None:
+        if args.symbol is None:
+            symbols = DEFAULT
+        else:
+            symbols = args.symbol.split(",")
+
+        for symbol in symbols:
+            stock = Printing(symbol)
+            stock.get_financials(args.quarterly)
+
+    #
+    # recs
+    # Stock recommendations.
+    #
+
+    def _add_get_recs(
+        self, subparsers: "argparse._SubParsersAction[argparse.ArgumentParser]"
+    ) -> None:
+        subparser = subparsers.add_parser("recs", help="Gets recs information.")
+        subparser.add_argument(
+            "--symbol",
+            required=False,
+            help=f"The ticker symbol to look at. Looks at {DEFAULT} if not set.",
+        )
+        subparser.set_defaults(func=self._do_get_recs)
+
+    def _do_get_recs(self, args: argparse.Namespace) -> None:
+        if args.symbol is None:
+            symbols = DEFAULT
+        else:
+            symbols = args.symbol.split(",")
+
+        for symbol in symbols:
+            stock = Printing(symbol)
+            stock.get_recommendations()
+
+
+def printing_cli() -> None:
+    cli = PrintingCli()
+    parser = cli.get_arg_parser()
+    args = parser.parse_args()
+    args.func(args)
+
+
+def main() -> None:
+    printing_cli()
--- src/printing/printing.py
+++ src/printing/printing.py
@@ -1,16 +1,16 @@
-import argparse
 import logging
 from datetime import datetime
 from enum import Enum
 from pprint import pprint
 
+import pandas as pd
+import pyspark.sql.functions as F
 from pandas import DataFrame
+from pyspark.sql import DataFrame, SparkSession
 from yfinance import Ticker
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.INFO)
-
-DEFAULT = ["MSFT", "AAPL", "TSLA", "NVDA"]
 
 
 class OptionType(Enum):
@@ -25,6 +25,11 @@ class Printing:
         self._symbol = symbol
 
     @property
+    def spark(self) -> SparkSession:
+        spark_session = SparkSession.builder.appName(self._symbol).getOrCreate()
+        return spark_session
+
+    @property
     def ticker(self) -> Ticker:
         LOGGER.info(f"Getting stock information for {self._symbol}")
         return Ticker(self._symbol)
@@ -32,6 +37,13 @@ class Printing:
     @property
     def today(self) -> str:
         return datetime.today().strftime("%Y-%m-%d")
+
+    @staticmethod
+    def display_df(df: DataFrame, count: int | None) -> None:
+        if count is None:
+            count = df.count()  # show all rows by default
+
+        df.show(count, False)
 
     def get_history(self, lookback_months: int = 1) -> DataFrame:
         """Get historical market data."""
@@ -66,8 +78,8 @@ class Printing:
             )
 
         for n in news:
-            print(f"{n["title"]}")
-            print(f"{n["url"]}")
+            print(f"{n["title"]}\n")
+            print(f"{n["url"]}\n")
             print(f"Related to: {n["related_tickers"]}")
             print("===================================================")
 
@@ -76,7 +88,7 @@ class Printing:
     def get_options_data(self, type: OptionType, date: str | None = None):
         """Get option chain for specific expiration.
 
-        Defaults to today
+        Defaults to today.
         """
         _date = date or self.today
         options = self.ticker.option_chain(_date)
@@ -89,16 +101,41 @@ class Printing:
         """Gets the full information for this company."""
         pprint(self.ticker.info)
 
-    def get_insider_info(self) -> None:
+    def get_insider_info(self, type: str | None, count: int | None) -> None:
+        if type == "buy":
+            LOGGER.info("Getting insider purchases...")
+            self._get_insider_purchases(count)
+
+        if type == "transaction":
+            LOGGER.info("Getting insider transactions...")
+            return self._get_insider_transactions(count)
+
         # pprint(self.ticker.institutional_holders)
-        print("Insider purchases:")
-        pprint(self.ticker.insider_purchases)
+        # print("\n\nInsider roster holders:")
+        # pprint(self.ticker.insider_roster_holders)
 
-        print("\n\nInsider roster holders:")
-        pprint(self.ticker.insider_roster_holders)
+    def _get_insider_purchases(self, count: int | None) -> DataFrame:
+        """Get latest insider transaction data."""
+        df = self.spark.createDataFrame(self.ticker.insider_purchases)
 
-        print("\n\nInsider transactions:")
-        pprint(self.ticker.insider_transactions)
+        self.display_df(df, count)
+        return df
+
+    def _get_insider_transactions(self, count: int | None) -> DataFrame:
+        """Get latest insider transaction data."""
+        df = self.spark.createDataFrame(self.ticker.insider_transactions)
+
+        df = df.select(
+            F.col("Shares").alias("share_count"),
+            F.col("Value"),
+            F.col("Text").alias("transaction_description"),
+            F.col("Insider").alias("name"),
+            F.col("Position").alias("job_title"),
+            F.col("Start Date").alias("transaction_date"),
+        ).withColumn("USD", F.format_number("Value", 0))
+
+        self.display_df(df, count)
+        return df
 
     def get_financials(self, is_quarterly: bool = False) -> None:
         """Show financial data.
@@ -143,167 +180,3 @@ class Printing:
     # def earnings_date(self):
     #     """Show future and historic earnings dates, returns at most next 4 quarters and last 8 quarters by default."""
     #     self.ticker.earnings_dates
-
-
-class PrintingCli:
-
-    def get_arg_parser(self) -> argparse.ArgumentParser:
-        parser = argparse.ArgumentParser()
-        subparsers = parser.add_subparsers(
-            metavar="cmd", help="The endpoint to hit.", required=True
-        )
-        self._add_get_price(subparsers)
-        self._add_get_news(subparsers)
-        self._add_get_insider_info(subparsers)
-        self._add_get_financials(subparsers)
-        self._add_get_recs(subparsers)
-
-        return parser
-
-    #
-    # price
-    # Get stock price information.
-    #
-
-    def _add_get_price(
-        self, subparsers: "argparse._SubParsersAction[argparse.ArgumentParser]"
-    ) -> None:
-        subparser = subparsers.add_parser("price", help="Gets the current stock price.")
-        subparser.add_argument(
-            "--symbol",
-            required=False,
-            help=f"The ticker symbol to look at. Looks at {DEFAULT} if not set.",
-        )
-        subparser.set_defaults(func=self._do_get_price)
-
-    def _do_get_price(self, args: argparse.Namespace) -> None:
-        if args.symbol is None:
-            symbols = DEFAULT
-        else:
-            symbols = args.symbol.split(",")
-
-        for symbol in symbols:
-            stock = Printing(symbol)
-            pprint(stock.price)
-
-    #
-    # news
-    # Latest news on the stock.
-    #
-
-    def _add_get_news(
-        self, subparsers: "argparse._SubParsersAction[argparse.ArgumentParser]"
-    ) -> None:
-        subparser = subparsers.add_parser("news", help="Gets latest news.")
-        subparser.add_argument(
-            "--symbol",
-            required=False,
-            help=f"The ticker symbol to look at. Looks at {DEFAULT} if not set.",
-        )
-        subparser.set_defaults(func=self._do_get_news)
-
-    def _do_get_news(self, args: argparse.Namespace) -> None:
-        if args.symbol is None:
-            symbols = DEFAULT
-        else:
-            symbols = args.symbol.split(",")
-
-        for symbol in symbols:
-            stock = Printing(symbol)
-            stock.news
-
-    #
-    # insider
-    # Latest insider information on the stock.
-    #
-
-    def _add_get_insider_info(
-        self, subparsers: "argparse._SubParsersAction[argparse.ArgumentParser]"
-    ) -> None:
-        subparser = subparsers.add_parser("insider", help="Gets insider information.")
-        subparser.add_argument(
-            "--symbol",
-            required=False,
-            help=f"The ticker symbol to look at. Looks at {DEFAULT} if not set.",
-        )
-        subparser.set_defaults(func=self._do_get_insider_info)
-
-    def _do_get_insider_info(self, args: argparse.Namespace) -> None:
-        if args.symbol is None:
-            symbols = DEFAULT
-        else:
-            symbols = args.symbol.split(",")
-
-        for symbol in symbols:
-            stock = Printing(symbol)
-            stock.get_insider_info()
-
-    #
-    # financials
-    # Stock recommendations.
-    #
-
-    def _add_get_financials(
-        self, subparsers: "argparse._SubParsersAction[argparse.ArgumentParser]"
-    ) -> None:
-        subparser = subparsers.add_parser(
-            "financials", help="Gets financial information."
-        )
-        subparser.add_argument(
-            "--symbol",
-            required=False,
-            help=f"The ticker symbol to look at. Looks at {DEFAULT} if not set.",
-        )
-        subparser.add_argument(
-            "--quarterly",
-            action="store_true",
-            help=f"Get the quarterly report if set to True.",
-        )
-        subparser.set_defaults(func=self._do_get_financials)
-
-    def _do_get_financials(self, args: argparse.Namespace) -> None:
-        if args.symbol is None:
-            symbols = DEFAULT
-        else:
-            symbols = args.symbol.split(",")
-
-        for symbol in symbols:
-            stock = Printing(symbol)
-            stock.get_financials(args.quarterly)
-
-    #
-    # recs
-    # Stock recommendations.
-    #
-
-    def _add_get_recs(
-        self, subparsers: "argparse._SubParsersAction[argparse.ArgumentParser]"
-    ) -> None:
-        subparser = subparsers.add_parser("recs", help="Gets recs information.")
-        subparser.add_argument(
-            "--symbol",
-            required=False,
-            help=f"The ticker symbol to look at. Looks at {DEFAULT} if not set.",
-        )
-        subparser.set_defaults(func=self._do_get_recs)
-
-    def _do_get_recs(self, args: argparse.Namespace) -> None:
-        if args.symbol is None:
-            symbols = DEFAULT
-        else:
-            symbols = args.symbol.split(",")
-
-        for symbol in symbols:
-            stock = Printing(symbol)
-            stock.get_recommendations()
-
-
-def printing_cli() -> None:
-    cli = PrintingCli()
-    parser = cli.get_arg_parser()
-    args = parser.parse_args()
-    args.func(args)
-
-
-def main() -> None:
-    printing_cli()
